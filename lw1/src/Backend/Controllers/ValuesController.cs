dfe8b1b6140b6f6892e63c83e4af153132278a95
redrik234 <illarionow.artem@yandex.ru>
2018-03-11 17:09:58 +0300

    add lw1

---
+++ lw1/src/Backend/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        static readonly ConcurrentDictionary<string, string> _data = new ConcurrentDictionary<string, string>();
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(string id)
+        {
+            string value = "";
+            _data.TryGetValue(id, out value);
+            Console.WriteLine(value);
+            return value;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public string Post([FromForm]string value)
+        {
+            //Debug.Assert(value != null, "String is null");
+            if(value == null)
+            {
+                return "string is null";
+            }
+            var id = Guid.NewGuid().ToString();
+            _data[id] = value;
+            return id;
+        }
+
+    }
+}
