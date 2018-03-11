fbc7c4b1c136b7327ac9f2c4c60dc4f30e261aee
redrik234 <illarionow.artem@yandex.ru>
2018-03-11 21:21:49 +0300

    lw2

---
+++ lw2/src/Backend/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Backend
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            var mainDir = Directory.GetParent(Directory.GetCurrentDirectory());
+            var configPath = mainDir + "/config";
+            if (!Directory.Exists(configPath))
+            {
+                throw new DirectoryNotFoundException("Not found configuration file");
+            }
+            DirectoryInfo dirConfig = new DirectoryInfo(configPath);
+                var config = new ConfigurationBuilder()
+                .SetBasePath(dirConfig.FullName)
+                .AddJsonFile("config_backend.json")
+                .Build();
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseConfiguration(config)
+                .UseContentRoot(Directory.GetCurrentDirectory())                
+                .UseStartup<Startup>()
+                .Build();
+            return host;
+        }
+    }
+}
---
+++ lw2/src/build.cmd
@@ -0,0 +1,17 @@
+@echo off
+if %~1 == "" goto err
+
+start /wait /d Frontend dotnet publish -o ..\..\%~1\Frontend
+start /wait /d Backend dotnet publish -o ..\..\%~1\Backend
+
+start /wait xcopy config ..\%~1\config /I
+start /wait xcopy run.cmd ..\%~1
+start /wait xcopy stop.cmd ..\%~1
+
+echo "compilation and build complete"
+pause
+exit 0
+
+:err
+echo "Not fount argument"
+exit 1
